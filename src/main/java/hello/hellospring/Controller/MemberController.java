9ee9d32e764a4e46177ce06830b4163e397e8fb0
halfmoonrain <junwoo1357@gmail.com>
2022-05-22 09:08:00 +0900

    user regist

--- src/main/java/hello/hellospring/Controller/MemberController.java
+++ src/main/java/hello/hellospring/Controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.Controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,21 @@ public class MemberController {
     @Autowired // Member Service와 연결시킬 때 사용해준다.
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")//데이터를 담아서 이동시킬 때는 post 방식을 쓰고
+                                //get은 조회할때 자주 쓴다  등록할때 : post 조회할때  : get
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }

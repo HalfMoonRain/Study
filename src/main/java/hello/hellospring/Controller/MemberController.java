88dc19d3e6f11092b6ce80a69118a99baf758dca
halfmoonrain <junwoo1357@gmail.com>
2022-05-22 20:08:32 +0900

    JDBC

--- src/main/java/hello/hellospring/Controller/MemberController.java
+++ src/main/java/hello/hellospring/Controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -17,19 +20,27 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/members/new")
+    @GetMapping(value = "/members/new")
     public String createForm() {
         return "members/createMemberForm";
     }
 
-    @PostMapping("/members/new")//데이터를 담아서 이동시킬 때는 post 방식을 쓰고
+    @PostMapping(value = "/members/new")//데이터를 담아서 이동시킬 때는 post 방식을 쓰고
                                 //get은 조회할때 자주 쓴다  등록할때 : post 조회할때  : get
     public String create(MemberForm form){
+
         Member member = new Member();
         member.setName(form.getName());
 
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }

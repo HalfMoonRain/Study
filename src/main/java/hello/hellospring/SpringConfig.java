781cedc228b4be15b4228edd446428cbd748480f
halfmoonrain <junwoo1357@gmail.com>
2022-05-23 14:25:32 +0900

    spinrgdataJPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -17,33 +17,14 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-        /*private final DataSource dataSource;
+        private final MemberRepository memberRepository;
 
-        public SpringConfig(DataSource dataSource) {
-                this.dataSource = dataSource;
+        public SpringConfig(MemberRepository memberRepository) {
+                this.memberRepository = memberRepository;
         }
-        */
-
-
-        private EntityManager em;
-
-        @Autowired
-        public SpringConfig(EntityManager em) {
-                this.em = em;
-        }
-
 
         @Bean
         public MemberService memberService() {
-                return new MemberService(memberRepository());
-
-        }
-
-        @Bean
-        public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        //        return new JdbcMemberRepository(dataSource);
-        //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JPAMemberRepositoy(em);
+                return new MemberService(memberRepository);
         }
 }
